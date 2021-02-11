5df51b4a398e0da6442e070b34252e0945b9b398
Durvish2911 <72204035+Durvish2911@users.noreply.github.com>
2021-02-11 20:47:54 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,7 @@
+function adduser(){
+    p1=document.getElementById("player1_name_input").value;
+    p1=document.getElementById("player2_name_input").value;
+    localStorage.setItem("player1_name",p1);
+    localStorage.setItem("player2_name".p2);
+    window.location="index.html";
+}
